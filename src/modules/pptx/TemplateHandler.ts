0c3d8b5c20161eafb79ec27377b637412fff0ed3
gabriel <ssgabrieldev@gmail.com>
2025-04-27 10:08:11 -0300

    new: template handler

---
+++ src/modules/pptx/TemplateHandler.ts
@@ -0,0 +1,23 @@
+import { TemplateHandler } from "../../abstracts/TemplateHandler";
+
+import {PPTXSlide} from "./Slide";
+
+export class PPTXTemplateHandler extends TemplateHandler {
+  private slides: PPTXSlide[] = [];
+
+  private getSlides(): PPTXSlide[] {
+
+  }
+
+  public addSlide(slide: PPTXSlide): void {
+
+  }
+
+  public render(data: object): void {
+      
+  }
+
+  public save(path: string): void {
+      
+  }
+}
