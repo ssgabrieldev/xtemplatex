ea6594c6229b39de091949e92e4caa2a3d25fa52
gabriel <ssgabrieldev@gmail.com>
2025-04-27 10:02:38 -0300

    new: xml utils

---
+++ src/utils/Xml.ts
@@ -0,0 +1,28 @@
+import { DOMParser, XMLSerializer } from "xmldom";
+
+export class UtilsXml {
+  static parser = new DOMParser();
+  static serializer = new XMLSerializer();
+
+  static getParentNodeByTag(tag: string, node: Node): Node | null {
+    let current: Node | null = node;
+
+    while (current) {
+      if (current.nodeName == tag) {
+        return current;
+      }
+
+      current = current.parentNode;
+    }
+
+    return null;
+  }
+
+  static getChildNodesByTag(tag: string, node: Node) {
+    return (node as Document).getElementsByTagName(tag);
+  }
+
+  static string(xml: Document): string {
+    return UtilsXml.serializer.serializeToString(xml);
+  }
+}
